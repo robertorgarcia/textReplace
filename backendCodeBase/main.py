1d8f6e8b59e0f90585a2c9fc59e5ec8c6e26d5d0
Roberto Garcia <rgarciaescallon@deloitte.com>
2022-02-27 22:02:43 +0100

    working end-to-end

--- backendCodeBase/main.py
+++ backendCodeBase/main.py
@@ -7,7 +7,14 @@ import json
 
 def lambda_handler(event, context):
     print(event)
+    body = json.loads(event['body'])
+    body = body['textbody']
+    bodyOut = body.replace("Oracle","Oracle©")
+    bodyOut = bodyOut.replace("Google","Google©")
+    bodyOut = bodyOut.replace("Microsoft","Microsoft©")
+    bodyOut = bodyOut.replace("Amazon","Amazon©")
+    bodyOut = bodyOut.replace("Deloitte","Deloitte©")
     return {
         'statusCode': 200,
-        'body': json.dumps({"message":"yas"})
+        'body': json.dumps({"message":bodyOut})
     }
