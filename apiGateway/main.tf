d43470032d0b2060eb740dac040e8dcdbabce470
Roberto Garcia <rgarciaescallon@deloitte.com>
2022-02-27 23:53:17 +0100

    adding stage and deployment

--- apiGateway/main.tf
+++ apiGateway/main.tf
@@ -9,3 +9,21 @@ resource "aws_api_gateway_rest_api" "rest_api" {
     types = ["REGIONAL"]
   }
 }
+
+resource "aws_api_gateway_deployment" "rest_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+
+  triggers = {
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.rest_api.body))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "rest_stage" {
+  deployment_id = aws_api_gateway_deployment.rest_deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+  stage_name    = "prod"
+}
