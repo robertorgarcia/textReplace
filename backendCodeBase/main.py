8b972be5711d9b4000d8e8ae30fb32b5db45fe12
Roberto Garcia <rgarciaescallon@deloitte.com>
2022-02-27 23:34:18 +0100

    prettier setup for text replacement logic

--- backendCodeBase/main.py
+++ backendCodeBase/main.py
@@ -3,17 +3,21 @@ import urllib
 import os
 import sys
 import json
+from functools import reduce
 
 
 def lambda_handler(event, context):
     print(event)
+    # extract text to be replaced - following the message schema provided by AWS and our OpenAPI schema
     body = json.loads(event['body'])
-    body = body['textbody']
-    bodyOut = body.replace("Oracle","Oracle©")
-    bodyOut = bodyOut.replace("Google","Google©")
-    bodyOut = bodyOut.replace("Microsoft","Microsoft©")
-    bodyOut = bodyOut.replace("Amazon","Amazon©")
-    bodyOut = bodyOut.replace("Deloitte","Deloitte©")
+    bodyOut = body['textbody']
+
+    # setup replacement chain
+    replacements = (('Oracle','Oracle©'),('Google','Google©'),('Microsoft','Microsoft©'),('Amazon','Amazon©'),('Deloitte','Deloitte©'))
+    for i in replacements:
+        bodyOut = bodyOut.replace(*i)
+
+    # setup return message as per OpenAPI schema spec
     return {
         'statusCode': 200,
         'body': json.dumps({"message":bodyOut})
